8ea1774e1b3376790c588036fe2db91c1f903d9a
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2021-04-11 19:03:15 +0200

    t

---
+++ tests/test_docker.py
@@ -0,0 +1,12 @@
+import pytest
+
+class Test():
+
+    def setup_method(self, test_method):
+        pass
+
+    def teardown_method(self, test_method):
+        pass
+
+    def test_noget(self):
+        pass
